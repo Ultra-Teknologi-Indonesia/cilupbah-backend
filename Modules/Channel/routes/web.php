e2ea314948dbb2b32167a7ea2eb736027678177c
Darriel Markerizal <darrielmarkerizal@gmail.com>
2026-06-05 11:12:48 +0700

    channel ui

--- Modules/Channel/routes/web.php
+++ Modules/Channel/routes/web.php
@@ -1,3 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Modules\Channel\Http\Controllers\ChannelController;
+
+Route::get('/channels', [ChannelController::class, 'index'])->name('channel.index');
+Route::post('/channels/shop/{id}/disconnect', [ChannelController::class, 'disconnectShop'])->name('channel.shop.disconnect');
+Route::post('/channels/shop/{id}/refresh-token', [ChannelController::class, 'refreshShopToken'])->name('channel.shop.refresh-token');
