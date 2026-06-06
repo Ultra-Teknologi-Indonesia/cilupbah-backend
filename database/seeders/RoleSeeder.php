17c0c42d2dfc21f92994ada42e2d37a4effc27a2
Darriel Markerizal <darrielmarkerizal@gmail.com>
2026-06-06 21:54:59 +0700

    chore: update role descriptions to be responsibility-focused

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -15,18 +15,18 @@ class RoleSeeder extends Seeder
     public function run(): void
     {
         $roles = [
-            'owner' => 'Pemilik Sistem',
-            'admin' => 'Administrator Sistem',
-            'purchasing' => 'Staff Pembelian',
-            'warehouse' => 'Staff Gudang',
-            'picker' => 'Staff Picker Gudang',
-            'checker' => 'Staff Checker Gudang',
-            'handover' => 'Staff Serah Terima Gudang',
-            'cs marketplace' => 'Customer Service Marketplace',
-            'putaway' => 'Staff Penempatan Gudang',
-            'kepala gudang' => 'Kepala Gudang',
-            'leader outbound' => 'Leader Outbound Gudang',
-            'leader inbound' => 'Leader Inbound Gudang'
+            'owner' => 'Bertanggung jawab atas seluruh sistem dan memiliki otorisasi penuh tanpa batas.',
+            'admin' => 'Bertanggung jawab atas pengelolaan administrasi keseluruhan sistem aplikasi.',
+            'purchasing' => 'Bertanggung jawab untuk melakukan pembelian stok barang dan negosiasi dengan supplier.',
+            'warehouse' => 'Bertanggung jawab atas pengelolaan operasional harian di dalam gudang.',
+            'picker' => 'Bertanggung jawab untuk mengambil barang dari rak penyimpanan sesuai dengan detail pesanan.',
+            'checker' => 'Bertanggung jawab untuk memeriksa kualitas, kuantitas, dan kesesuaian barang sebelum dipacking.',
+            'handover' => 'Bertanggung jawab untuk menyerahkan paket pesanan yang sudah dikemas kepada pihak kurir/ekspedisi.',
+            'cs marketplace' => 'Bertanggung jawab untuk melayani pertanyaan, membalas chat, dan menangani komplain pelanggan di marketplace.',
+            'putaway' => 'Bertanggung jawab untuk menyimpan dan memindahkan barang yang baru datang ke rak penyimpanan yang tepat.',
+            'kepala gudang' => 'Bertanggung jawab penuh untuk memimpin, mengawasi, dan memastikan efisiensi seluruh aktivitas gudang.',
+            'leader outbound' => 'Bertanggung jawab untuk mengoordinasikan tim dalam proses pemenuhan dan pengeluaran barang (pesanan).',
+            'leader inbound' => 'Bertanggung jawab untuk mengoordinasikan tim dalam proses penerimaan dan pengecekan barang masuk.'
         ];
 
         foreach ($roles as $role => $description) {
