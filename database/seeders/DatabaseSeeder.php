4923f08ab3e660f114b1365bb3d0e8bb1fd6e5cf
Darriel Markerizal <darrielmarkerizal@gmail.com>
2026-06-07 14:42:11 +0700

    chore: remove ProductDatabaseSeeder from main DatabaseSeeder to prevent redundant seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -29,7 +29,6 @@ class DatabaseSeeder extends Seeder
 
         $this->call(\Modules\Region\Database\Seeders\RegionDatabaseSeeder::class);
         $this->call(\Modules\Channel\Database\Seeders\ChannelDatabaseSeeder::class);
-        $this->call(\Modules\Product\Database\Seeders\ProductDatabaseSeeder::class);
         $this->call(\Modules\Warehouse\Database\Seeders\WarehouseDatabaseSeeder::class);
     }
 }
