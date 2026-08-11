7f80dadf11146c93a90a5fd877553e5ea44ec39b
Darriel Markerizal <darrielmarkerizal@gmail.com>
2026-08-11 17:12:01 +0700

    chore: add mixin docblock to LocationBinResource to fix IDE property access warnings

--- Modules/Warehouse/app/Http/Resources/LocationBinResource.php
+++ Modules/Warehouse/app/Http/Resources/LocationBinResource.php
@@ -5,6 +5,9 @@ namespace Modules\Warehouse\Http\Resources;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * @mixin \Modules\Warehouse\Models\LocationBin
+ */
 class LocationBinResource extends JsonResource
 {
     public function toArray(Request $request): array
