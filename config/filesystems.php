e4fc68eb6e019e91fb16df4796dce98f1bace936
Darriel Markerizal <darrielmarkerizal@gmail.com>
2026-08-20 16:02:04 +0700

    feat(storage): support S3 for documents disk on production cluster

--- config/filesystems.php
+++ config/filesystems.php
@@ -23,7 +23,18 @@ return [
             'report' => false,
         ],
 
-        'documents' => [
+        'documents' => env('FILESYSTEM_DISK') === 's3' || env('DOCUMENTS_DISK_DRIVER') === 's3' ? [
+            'driver' => 's3',
+            'key' => env('AWS_ACCESS_KEY_ID'),
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'region' => env('AWS_DEFAULT_REGION'),
+            'bucket' => env('AWS_BUCKET'),
+            'url' => env('AWS_URL'),
+            'endpoint' => env('AWS_ENDPOINT'),
+            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+            'throw' => false,
+            'report' => false,
+        ] : [
             'driver' => 'local',
             'root' => storage_path('app/documents'),
             'throw' => false,
